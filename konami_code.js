9445ec08335c0ff56c1546e52a2642ce1a398739
Christopher Michael Boujoukos <cboujoukos@gmail.com>
2017-10-28 16:07:27 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,18 +1,18 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-let i = 0;
+let index = 0;
 function init() {
   // Write your JavaScript code inside the init() function
   document.body.addEventListener('keydown', (e) => {
     const key = parseInt(e.detail || e.which);
-    if (key === code[i]) {
-      i++;
-      if (i === code.length) {
+    if (key === code[index]) {
+      index++;
+      if (index === code.length) {
         alert('Congratulations!');
 
-        i = 0;
+        index = 0;
       }
     } elese {
-      i = 0;
+      index = 0;
     }
   });
 }
