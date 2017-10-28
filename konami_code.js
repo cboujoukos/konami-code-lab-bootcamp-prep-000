8322021c43a16d3b4532f4c33d1f805828f8190b
Christopher Michael Boujoukos <cboujoukos@gmail.com>
2017-10-28 16:00:20 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,5 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-let i = 0
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+let i = 0;
 function init() {
   // Write your JavaScript code inside the init() function
   document.body.addEventListener('keydown', (e) => {
